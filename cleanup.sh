2f77ddea775ecd29507e5bae2abe910db895b8ca
Jason Gellatly <jason.gellatly@gmail.com>
2020-04-05 15:21:59 +1000

    Clean up after ourselves

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -e "\e[31mCleaning up any xyzjace/ containers\e[0m"
+running_xyzjace_containers=$(docker ps | grep 'xyzjace/' | cut -d " " -f1)
+for container in ${running_xyzjace_containers}; do
+  docker kill ${container}
+  docker rm ${container}
+done
+
+echo -e "\e[31mCleaning up any xyzjace/ images\e[0m"
+xyzjace_images=$(docker images | grep xyzjace/ | cut -d " " -f1)
+for image in ${running_xyzjace_images}; do
+  docker rmi -f ${image}
+done
