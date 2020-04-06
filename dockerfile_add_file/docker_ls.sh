a727c9459b79dbc216c918c50bd664d5021475e4
Jason Gellatly <jgellatly@deputy.com>
2020-04-06 18:38:02 +1000

    Simplify the add_file Dockerfile

--- dockerfile_add_file/docker_ls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo -e "\033[1m\033[34mUse the docker run command to start the image as a shell instead\033[0m\n"
-echo -e "\033[1m\033[34m$ docker run -it dockerfile:add_file /bin/sh\033[0m\n"
-docker run -it dockerfile:add_file /bin/sh
