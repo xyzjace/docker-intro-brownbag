18131b880e0ca4053be080441528f28fc3baac39
Jason Gellatly <jgellatly@deputy.com>
2020-04-06 19:39:33 +1000

    Stop dockerfile containers

--- cleanup.sh
+++ cleanup.sh
@@ -12,6 +12,12 @@ for image in ${xyzjace_images}; do
   docker rmi -f ${image}
 done
 
+echo -e "\033[31mCleaning up any dockerfile containers\033[0m"
+running_dockerfile_containers=$(docker ps | grep 'dockerfile' | cut -d " " -f1)
+for container in ${running_dockerfile_containers}; do
+  docker kill ${container}
+done
+
 echo -e "\033[31mCleaning up any dockerfile: images\033[0m"
 dockerfile_images=$(docker images | grep dockerfile | tr -s " " | cut -d " " -f1,2 | sed -e 's/ /\:/g')
 for image in ${dockerfile_images}; do
