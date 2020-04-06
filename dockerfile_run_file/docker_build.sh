458b69172585026d92e0292647889e0d499f84cc
Jason Gellatly <jgellatly@deputy.com>
2020-04-06 18:29:52 +1000

    Introduce Dockerfile

---
+++ dockerfile_run_file/docker_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+echo -e "\033[1m\033[34mUse the docker build command to build images\033[0m\n"
+echo -e "\033[1m\033[34m$ docker build -t dockerfile:run_file .\033[0m\n"
+docker build -t dockerfile:run_file .
---
+++ dockerfile_run_file/docker_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+echo -e "\033[1m\033[34mUse the docker run command to run a container\033[0m\n"
+echo -e "\033[1m\033[34m$ docker run dockerfile:run_file\033[0m\n"
+docker run dockerfile:run_file
