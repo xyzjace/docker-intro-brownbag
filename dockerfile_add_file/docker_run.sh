1fde7e14b56f58a07bf536868024956b98e203c6
Jason Gellatly <jgellatly@deputy.com>
2020-04-06 18:39:52 +1000

    Fixes

--- dockerfile_add_file/docker_run.sh
+++ dockerfile_add_file/docker_run.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo -e "\033[1m\033[34mUse the docker run command to run a container\033[0m\n"
-echo -e "\033[1m\033[34m$ docker run --rm --name dockerfile-add-file -d dockerfile:add_file\033[0m\n"
-docker run --rm --name dockerfile-add-file -d dockerfile:add_file
+echo -e "\033[1m\033[34m$ docker run --rm --name dockerfile-add-file -it dockerfile:add_file\033[0m\n"
+docker run --rm --name dockerfile-add-file -it dockerfile:add_file
--- dockerfile_intro/docker_run.sh
+++ dockerfile_intro/docker_run.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo -e "\033[1m\033[34mUse the docker run command to run a container\033[0m\n"
-echo -e "\033[1m\033[34m$ docker run --rm --name dockerfile-intro -d dockerfile:intro\033[0m\n"
+echo -e "\033[1m\033[34m$ docker run --rm --name dockerfile-intro -it dockerfile:intro\033[0m\n"
 docker run --rm --name dockerfile-intro -it dockerfile:intro
